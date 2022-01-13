b570089fca9cb40f51c9cef5af903371f26e4914
alex <alex@pdp7.net>
2022-01-13 20:31:30 +0100

    Document how evaluate works

--- src/score.rs
+++ src/score.rs
@@ -31,6 +31,11 @@ impl std::fmt::Display for CharScore {
 pub fn evaluate<const N: usize>(guess: [char; N], solution: [char; N]) -> [CharScore; N] {
     let mut result: [CharScore; N] = [CharScore::NotFound; N];
 
+    // For guess abcde and solution aaabb, we construct a map:
+    //
+    // a => ([0], {0,1,2}), where [0] is where guess contains a, and {0,1,2} is where solution contains a
+    // b => ([1], {3, 4}), ...
+    // ...
     let mut x: HashMap<char, (Vec<usize>, HashSet<usize>)> = HashMap::new();
     for (i, (g, s)) in guess
         .iter()
@@ -46,6 +51,22 @@ pub fn evaluate<const N: usize>(guess: [char; N], solution: [char; N]) -> [CharS
             .insert(i);
     }
 
+    // Now we can use that map to identify Correct matches; for example, with:
+    //
+    // a => ([0], {0,1,2})
+    //
+    // guess contains an a in position 0, and solution does too, so we can mark position 0 as Correct
+    //
+    // While we do that, we construct another dict for the following phase, removing the Correct characters.
+    //
+    // So the above transforms into:
+    //
+    // a => ([], {1,2})
+    //
+    // Meaning that the guess contains no as, and the solution has 2 at positions 1 and 2.
+    //
+    // However, we don't really care about the positions on the solution any more, so instead of a set,
+    // we just score a count.
     let mut y: HashMap<char, (Vec<usize>, usize)> = HashMap::new();
     for (c, (gps, sps)) in &x {
         let mut ngps: Vec<usize> = Vec::new();
@@ -64,6 +85,13 @@ pub fn evaluate<const N: usize>(guess: [char; N], solution: [char; N]) -> [CharS
         y.insert(*c, (ngps, nsps));
     }
 
+    // Now we have a map that maps each letter to the positions in the guess, and the quantity in solution
+    // (excluding correct matches). We use that to mark misplaced letters.
+    //
+    // a => ([1,2,3,4], 2)
+    //
+    // We don't really care about what letter we're talking about. We know there's two in the solution,
+    // so we mark the first two positions in the guess as Misplaced.
     for (gps, sps) in y.values() {
         for i in (Range {
             start: 0,
